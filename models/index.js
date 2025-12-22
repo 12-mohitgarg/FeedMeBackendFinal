f857233c8aaa89c538e54a24769fecc4def4ded7
Mohit garg <mohitgarggmail.com@Mohits-MacBook-Air.local>
2025-12-22 10:53:03 +0530

    de

--- models/index.js
+++ models/index.js
@@ -12,33 +12,42 @@ const db = {};
 
 console.log(process.env.DB_DEV_DATABASE); // Debug: Check if password is loaded
 
-const sequelize = new Sequelize(
-  process.env.DB_DEV_DATABASE,
-  process.env.DB_DEV_USERNAME,
-  process.env.DB_DEV_PASSWORD,
-  {
-    host: process.env.DB_DEV_HOST,
-    dialect: "mysql",
-    port: 3307,
-    logging: false,
-    dialectModule: mysql2,
-    timezone: process.env.DB_DEV_TIMEZONE,
+// const sequelize = new Sequelize(
+//   process.env.DB_DEV_DATABASE,
+//   process.env.DB_DEV_USERNAME,
+//   process.env.DB_DEV_PASSWORD,
+//   {
+//     host: process.env.DB_DEV_HOST,
+//     dialect: "mysql",
+//     port: 3307,
+//     logging: false,
+//     dialectModule: mysql2,
+//     timezone: process.env.DB_DEV_TIMEZONE,
 
-    pool: {
-      max: 1,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
+//     pool: {
+//       max: 1,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     },
 
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
-);
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//       }
+//     }
+//   }
+// );
+
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
 
 
 // Load all model files dynamically
